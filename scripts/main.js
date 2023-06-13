43cf049b10d09b1b5f2e1c929009e3c0a870f98d
oasoby <134684352+oasoby@users.noreply.github.com>
2023-06-13 18:25:04 +0900

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,18 @@
+import { world } from "@minecraft/server";
 
+world.beforeEvents.chatSend.subscribe(ev => {
+    const { sender, message } = ev;
+
+    if (sender.hasTag("test")) {
+        if (message === ".test") {
+            ev.cancel = true;
+            sender.runCommandAsync("say test!!");
+        } else if (message === ".test2") {
+            ev.cancel = true;
+            sender.runCommandAsync("say test2!");
+        } else if (1 === 1) {
+            ev.cancel = true;
+            sender.runCommandAsync(`tellraw @a {"rawtext":[{"text":"<§9${sender.name}§r> ${message}"}]}`)
+        }
+    }
+});
